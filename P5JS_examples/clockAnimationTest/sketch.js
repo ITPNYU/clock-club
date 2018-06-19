aeeb9def5eb9bffee69b4962fb871e4471e423fe
tigoe <tom.igoe@gmail.com>
2018-06-19 07:38:01 -0400

    updated clock animation test to adjust hour hand better

--- P5JS_examples/clockAnimationTest/sketch.js
+++ P5JS_examples/clockAnimationTest/sketch.js
@@ -11,22 +11,23 @@ Program works by rotating the drawing surface proportional to the angle
 of each hand, then drawing the line for the hand.
 
 created 22 Aug 2012
-modified for image layers 7 May 2018
+modified 19 Jun 2018
 by Tom Igoe
 */
 
-var hourImg;    // images to be displayed on each layer
+var hourImg;        // images to be displayed on each layer
 var minuteImg;
 var secondImg;
 var faceImg;
 
-var hAlpha = 255; // transparency of the layers, from 0 (invisible) to 255 (opaque)
+var hAlpha = 255;   // transparency of the layers, from 0 (invisible) to 255 (opaque)
 var mAlpha = 127;
 var sAlpha = 100;
 
-var h = 0;    // hour, minute, second
+var h = 0;          // hour, minute, second
 var m = 0;
 var s = 0;
+var oldM = 0;       // previous minute
 var speed = 60;     // frames per second
 function preload() {
   // load the images:
@@ -50,7 +51,7 @@ function draw() {
   // draw face
   image(faceImg, 0, 0);
   // draw hour layer:
-  drawHand(h, hourImg, hAlpha, 12);
+  drawHand(h, hourImg, hAlpha, 60);
   // draw minute layer:
   drawHand(m, minuteImg, mAlpha, 60);
   // draw second layer:
@@ -61,8 +62,12 @@ function draw() {
     m++;          // advance minutes
     s=0;          // reset secoonds
   }
-  if (m > 59) {   // on the top of the hour
+  if (m % 12 == 0 && m != oldM) {   // on the top of the hour
     h++;          // advance hours
+    oldM = m;     // save current minute for comparison
+  }
+  if (m > 59) {   // on the top of the hour
+  //  h++;          // advance hours
     m=0;          // reset minutes
   }
   if (h > 11) {   // on the top of the clock
