017c107cc8a5b0bdcdb972a5b7590eb740b4250d
Tom Igoe <tom.igoe@gmail.com>
2020-07-14 06:12:49 -0400

    Update sketch.js

--- P5JS_examples/SunMoonClock/sketch.js
+++ P5JS_examples/SunMoonClock/sketch.js
@@ -25,7 +25,7 @@ var now = new Date();
 var sunPos, moonPos, moonIllumination, times;
 
 // your position in the world. This determined all the suncalc data:
-var latitude =0;
+var latitude = 0;
 var longitude = 0;
 
 function setup() {
@@ -46,15 +46,15 @@ function setup() {
 }
 
 function draw() {
-  background('#000'); //black background
+  background('#333'); //black background
 
-// mark north on the map:
+  // mark north on the map:
   fill('#aaa');
   stroke('#aaa');
-  let x=height/2-clockRadius*2;
-  triangle(width/2, x, (width/2) + 5, x+20, (width/2) -5, x+20);
-  line(width/2, x+20, width/2, x+40);
-  text('N', width/2, x+60);
+  let x = height / 2 - clockRadius * 2;
+  triangle(width / 2, x, (width / 2) + 5, x + 20, (width / 2) - 5, x + 20);
+  line(width / 2, x + 20, width / 2, x + 40);
+  text('N', width / 2, x + 60);
 
   translate(width / 2, height / 2); // move to the center of the window
   rotate(3 * PI / 2); // rotate drawing 270 degrees to get 0 at the top
@@ -92,14 +92,14 @@ function calcPositions() {
   moonIllumination = SunCalc.getMoonIllumination(now);
   // get position of the sun (azimuth and altitude) at today's sunrise:
   sunPos = SunCalc.getPosition(now, latitude, longitude);
-  moonPos = SunCalc.getMoonPosition(now,  latitude, longitude);
+  moonPos = SunCalc.getMoonPosition(now, latitude, longitude);
 }
 
 // draw the sun at its azimuth:
 function drawSun(pos) {
-  var angle =  pos;
-  var x = -clockRadius*2 * cos(angle);
-  var y = -clockRadius*2 * sin(angle);
+  var angle = pos;
+  var x = -clockRadius * 2 * cos(angle);
+  var y = -clockRadius * 2 * sin(angle);
   noStroke();
   // dim the sun after sunset:
   if (now > times.sunrise && now < times.sunset) {
@@ -108,37 +108,39 @@ function drawSun(pos) {
     fill('#555500'); // at night, sun is dimmed
   }
   // draw the sun:
-  ellipse(x,y, sunRadius, sunRadius);
+  ellipse(x, y, sunRadius, sunRadius);
 }
 
 // draw the moon at its azimuth:
 function drawMoon(pos) {
-  var angle =  pos;
+  var angle = pos;
   var x = -clockRadius * cos(angle);
   var y = -clockRadius * sin(angle);
   noStroke();
   // brighten the moon after moonrise:
   if (now > times.night && now < times.sunriseEnd) {
-    fill('#444'); // during the day, moon is dimmed
+    fill('#444'); // during the day, moon is dimmed 
   } else {
     fill('#bbb'); // at night, moon is brighter
   }
   // rotate so zero is at the top:
-  rotate(3*PI/2);
+  rotate(3 * PI / 2);
   push();
   // draw the bright part of the moon:
-  ellipse(x,y, moonRadius, moonRadius);
+  ellipse(x, y, moonRadius, moonRadius);
   fill('#222');
+  // fullness is a percentage, and its sign indicates waning (+) or waxing(-):
   var fullness = map(moonIllumination.fraction, 0, 1, -moonRadius, moonRadius);
-  // if fullness  is negative (waxing moon), you need to adjust the shadow:
+  // if fullness is negative (waxing moon), you need to adjust the shadow:
   var adjustment = 1;
   if (fullness < 0) {
     adjustment = -adjustment;
   }
   // draw the leading edge of the shadow:
-  arc(x,y, fullness, moonRadius, PI/2, 3*PI/2, PI);
+  // arc: x, y, width, height, start, stop, mode
+  arc(x, y, fullness, moonRadius, PI / 2, 3 * PI / 2, PIE);
   // draw the back half of the shadow:
-  arc(x,y, adjustment*moonRadius+adjustment, moonRadius+1, 3*PI/2, PI/2, PI);
+  arc(x, y, adjustment * moonRadius + adjustment, moonRadius + 1, 3 * PI / 2, PI / 2, PIE);
   pop();
 }
 
